e4f0b6ac3c3620d697c994c7ad615bcd6bc343db
aisng <tasignas@gmail.com>
2022-12-16 10:08:05 +0200

    .net atsiskaitymas

--- Lec5/Program.cs
+++ Lec5/Program.cs
@@ -1,8 +1,1 @@
-﻿Console.WriteLine("testing the push");
-bool gitTestSuccess = true;
-if (gitTestSuccess == true)
-{
-    Console.WriteLine("pakeitimai įrašyti");
-}
-
-//komentaras
+﻿// atsiskaitymas
